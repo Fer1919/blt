1c9eb84f9ba12b453ebc3218a7780fefa2f2b32f
gitEstudio <ingespinosa1981@gmail.com>
2022-05-12 12:06:08 -0400

    Boleta

---
+++ index.js
@@ -0,0 +1,7 @@
+let dato, num, total;
+dato=+prompt('Ingrese el valor que cobró al cliente. Valor Neto.')
+num=.1225*dato;
+total=dato-num;
+alert(`Ud recibirá $${total}.- El Sii le retendrá $${num}.- APP NandoScipt`)
+
+
