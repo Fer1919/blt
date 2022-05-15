e47083371183d8230ab0be3e38840565d75155b5
gitEstudio <ingespinosa1981@gmail.com>
2022-05-14 21:09:21 -0400

    blt_14May

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let dato, num, total;
 dato=+prompt('Ingrese el valor que cobró al cliente. Valor Neto de la boleta.')
 num=.1225*dato;
 total=dato-num;
-alert(`Ud recibirá $${total}.- El Sii le retendrá $${num}.- APP NandoScipt`)
+
+
+ if (isNaN(dato)) {
+    alert('Ud. Ingreso un valor invalido / APP NandoScript')
+ }    
+
+
+  alert(`Ud recibirá $${total}.- El Sii le retendrá $${num}.- APP NandoScipt`)
 
 
